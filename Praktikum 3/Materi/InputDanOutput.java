676199f6fc3c34e4ca9a7a175a7dff07f4c840e2
RahmaOktoyadi19 <rahmaoktoyadi37f@gmail.com>
2025-09-08 01:53:11 +0700

    Add files via upload

---
+++ Praktikum 3/Materi/InputDanOutput.java
@@ -0,0 +1,37 @@
+package Praktikum3.Materi;
+import java.util.Scanner;
+
+public class InputDanOutput {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        System.out.println("Baris kedua");
+
+        System.out.print("Hello ");
+        System.out.print("World!");
+        System.out.println();
+
+        String nama = "Andri";
+        int umur = 19;
+        double tinggi = 165.5;
+
+        System.out.printf("Nama: %s, umur: %d, tinggi: %.1f cm%n", nama, umur, tinggi);
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Input String
+        System.out.print("Masukkan nama: ");
+        String inputNama = scanner.nextLine();
+
+        // Input Integer
+        System.out.print("Masukkan umur: ");
+        int inputUmur = scanner.nextInt();
+
+        // Input Double
+        System.out.print("Masukkan tinggi badan: ");
+        double inputTinggi = scanner.nextDouble();
+        scanner.close();
+
+        System.out.printf("Halo %s, umur kamu %d tahun, dan tinggi kamu %.1f cm%n", inputNama, inputUmur, inputTinggi);
+    }
+}
---
+++ Praktikum 3/Materi/OperatorBitwise.java
@@ -0,0 +1,15 @@
+package Praktikum3.Materi;
+
+public class OperatorBitwise {
+    public static void main(String[] args) {
+        int a = 12; // 1100 dalam biner
+        int b = 10; // 1010 dalam biner
+
+        System.out.println("a & b = " + (a & b)); // AND: 8 (1000)
+        System.out.println("a | b = " + (a | b)); // OR: 14 (1110)
+        System.out.println("a ^ b = " + (a ^ b)); // XOR: 6 (0110)
+        System.out.println("~a = " + (~a)); // NOT: -13
+        System.out.println("a << 1 = " + (a << 2)); // Shift Left: 48 (110000)
+        System.out.println("a >> 1 = " + (a >> 2)); // Shift Right: 3 (0011)
+    }
+}
