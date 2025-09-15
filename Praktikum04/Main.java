8994f99d6f61629c94208651f4100ccdb5a989f9
RahmaOktoyadi19 <rahmaoktoyadi37f@gmail.com>
2025-09-15 14:50:45 +0700

    Add files via upload

---
+++ Praktikum04/Main.java
@@ -0,0 +1,44 @@
+import  java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        // Meminta nilai input prisma segitiga
+        System.out.println("==== KALKULATOR PRISMA SEGITIGA ====");
+        System.out.print("Masukkan panjang alas segitiga (a): ");
+        double panjangAlas = input.nextDouble();
+
+        System.out.print("Masukkan sisi kedua segitiga (b): ");
+        double sisiKedua = input.nextDouble();
+
+        System.out.print("Masukkan sisi ketiga segitiga (c): ");
+        double sisiKetiga = input.nextDouble();
+
+        System.out.print("Masukkan tinggi segitiga alas: ");
+        double tinggiAlas = input.nextDouble();
+
+        System.out.print("Masukkan tinggi prisma (T): ");
+        double tinggiPrisma = input.nextDouble();
+        System.out.println();
+
+        // menghitung luas alas segitiga
+        double luasAlas = 0.5 * panjangAlas * tinggiAlas;
+
+        // menghitung volume prisma segitiga
+        double volume = luasAlas * tinggiPrisma;
+
+        // menghitung luas permukaan prisma segitiga
+        double kelilingAlas = panjangAlas + sisiKedua + sisiKetiga;
+        double luasPermukaan = (2 * luasAlas) + (kelilingAlas * tinggiPrisma);
+
+        // Menampilkan hasil
+        System.out.println("=== HASIL PERHITUNGAN ===");
+        System.out.println("Luas Alas Segitiga     = " + luasAlas);
+        System.out.println("Volume Prisma          = " + volume);
+        System.out.println("Luas Permukaan Prisma  = " + luasPermukaan);
+
+    }
+}
+
