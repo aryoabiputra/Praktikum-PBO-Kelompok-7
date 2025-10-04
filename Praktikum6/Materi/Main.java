83e09d7a751bac70cc70190c05bd6419b8b3cf8f
andriansyah28 <andriansyah01995@gmail.com>
2025-10-04 16:23:50 +0700

    Praktikum 6

---
+++ Praktikum6/Materi/Main.java
@@ -0,0 +1,28 @@
+package Praktikum6.Materi;
+
+public class Main {
+    public static void main(String[] args) {
+        Produk p1 = new Produk ("Laptop", 15000000, 5);
+        Produk p2 = new Produk ("Smartphone", 3500000, 10);
+
+        // System.out.println(p1.nama);
+        // System.out.println(p1.stok);
+        // System.out.println(p1.harga);
+
+        p1.tampilkanInfo();
+
+        // p1.namaSupplier(); // error karena private
+        p1.namaSupplierFix();
+        p2.tampilkanInfo();
+
+        System.out.println("Nama produk 1 : " + p1.nama);
+        // System.out.println(p1.harga); // error karena private
+
+        System.out.println("Harga  Produk 1 " +  p1.getHarga());
+
+        p1.setHarga(7000000);
+        System.out.println("Harga baru untuk produk 1 : " + p1.getHarga());
+
+        Produk.infoJumlahProduk();
+    }
+}
---
+++ Praktikum6/Materi/Produk.java
@@ -0,0 +1,52 @@
+package Praktikum6.Materi;
+
+public class Produk {
+    public String nama;
+    private double harga;
+    protected int stok;
+    private String namaSupplier = "Andri";
+
+    // Static variable
+    static int jumlahProduk = 0;
+
+
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+        jumlahProduk++;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("Nama  : " + nama);
+        System.out.println("Harga : " + harga);
+        System.out.println("Stok  : " + stok);
+    }
+
+    private void namaSupplier() {
+        System.out.println("Nama Supplier : " + namaSupplier);
+    }
+
+    public void namaSupplierFix(){
+        namaSupplier();
+    }
+
+    public double getHarga(){
+        return harga;
+    }
+
+    public void setHarga(double hargaBaru){
+        if(hargaBaru > 0){
+            this.harga = hargaBaru;
+        } else if (hargaBaru == 0 ){
+            this.harga = hargaBaru;
+            System.out.println("Produk ini gratis");
+        } else {
+            System.out.println("Harga produk tidak boleh negatif");
+        }
+    }
+
+    public static void infoJumlahProduk(){
+        System.out.println("Total produk yang telah dibuat :" + jumlahProduk);
+    }
+}
